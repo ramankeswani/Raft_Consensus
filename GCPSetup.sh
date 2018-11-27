284a6240275c413be2fb8d606018f1381f6e29af
Raman Keswani <ramankes@buffalo.edum>
2018-11-27 14:15:01 -0500

    GCP Setup Script

--- GCPSetup.sh
+++ GCPSetup.sh
@@ -1,8 +1,12 @@
 cd
 sudo wget https://storage.googleapis.com/golang/go1.10.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
+sudo tar -xzf go1.10.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin:${HOME}/go/bin
 cd go/src
+sudo mkdir github.com
+cd github.com
+sudo mkdir mattn
+cd mattn
 git clone https://github.com/mattn/go-sqlite3.git
 go build github.com/mattn/go-sqlite3
 cd
