f9e19f467bfea4cb52e26d65520c01d885f52dcb
Raman Keswani <ramankes@buffalo.edum>
2018-12-01 16:50:46 -0500

    DB change and recovery fix

--- run/run_process.sh
+++ run/run_process.sh
@@ -6,3 +6,4 @@ sudo git pull
 sudo go build
 sudo nohup ./Raft_Consensus $1 $2 0 2>&1 > console_$2.txt &
 sudo nohup ./Raft_Consensus $3 $4 0 2>&1 > console_$4.txt &
+sudo nohup ./Raft_Consensus $5 $6 0 2>&1 > console_$6.txt &
